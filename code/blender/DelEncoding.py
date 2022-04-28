b307f996314213c58e187c2288b38b6ba2a5287f
Mequam <blue9ja@gmail.com>
2022-04-28 15:22:23 -0400

    got the basic encoding working pushing for class consistency

--- code/blender/DelEncoding.py
+++ code/blender/DelEncoding.py
@@ -1,2 +1,18 @@
 #This file contains the data structures used in delauny triangulation
 #
+class Triangulation:
+	def __init__(self,points):
+		self.points = points	
+		#array of tuples
+		#where each element in the
+		#tuple corispoinds to 
+		#a point idx
+		self.triangles = {}
+		#an array of circomcircles
+		#takes a triangle (tuple
+		# of point idx) and returns
+		# the stored circomcircle
+		self.circles = {}
+	#adds a new point to the triangulation
+	def addPoint(self,point):
+		pass
